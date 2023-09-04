e8574d76ed1035628b0f6909e5bcae8bf3e17ee7
Temur Lakerbaya <lakerbaya00@mail.ru>
2023-09-04 14:08:29 +0300

    try scroll-block

--- script/script.js
+++ script/script.js
@@ -1,19 +1,19 @@
 const menuBtn = document.getElementsByClassName('header__menu-btn')[0];
 const mobMenu = document.getElementsByClassName('mob-menu')[0];
-const body = document.getElementsByClassName('body')[0];
+const html = document.getElementsByTagName('html')[0];
 
 menuBtn.addEventListener('click', () => {
     let modBtn = 'header__menu-btn_active';
     let modMenu = 'mob-menu_active';
-    let modBody = 'body_scroll-block';
+    let modhtml = 'html_scroll-block';
     if (menuBtn.classList.contains(modBtn)) {
         menuBtn.classList.remove(modBtn);
         mobMenu.classList.remove(modMenu);
-        body.classList.remove(modBody);
+        html.classList.remove(modhtml);
     } else {
         menuBtn.classList.add(modBtn);
         mobMenu.classList.add(modMenu);
-        body.classList.add(modBody);
+        html.classList.add(modhtml);
     }
 })
 
