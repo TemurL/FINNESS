fb7055dadca61874cdec09268e4ec24f1fd2541a
Temur Lakerbaya <lakerbaya00@mail.ru>
2023-09-07 01:17:27 +0300

    fix mob-menu

--- script/script.js
+++ script/script.js
@@ -1,20 +1,20 @@
 const menuBtn = document.getElementsByClassName('header__menu-btn')[0];
 const mobMenu = document.getElementsByClassName('mob-menu')[0];
 const html = document.getElementsByTagName('html')[0];
+const mobMenuListItems = mobMenu.children[0].children;
 
-menuBtn.addEventListener('click', () => {
-    let modBtn = 'header__menu-btn_active';
-    let modMenu = 'mob-menu_active';
-    let modhtml = 'html_scroll-block';
-    if (menuBtn.classList.contains(modBtn)) {
-        menuBtn.classList.remove(modBtn);
-        mobMenu.classList.remove(modMenu);
-        html.classList.remove(modhtml);
-    } else {
-        menuBtn.classList.add(modBtn);
-        mobMenu.classList.add(modMenu);
-        html.classList.add(modhtml);
-    }
-})
+let modBtn = 'header__menu-btn_active';
+let modMenu = 'mob-menu_active';
+let modhtml = 'html_scroll-block';
 
+const menuAct = () => {
+    menuBtn.classList.toggle(modBtn);
+    mobMenu.classList.toggle(modMenu);
+    html.classList.toggle(modhtml);
+}
 
+menuBtn.addEventListener('click', menuAct);
+
+for (let i = 0; i < mobMenuListItems.length; i++) {
+    mobMenuListItems[i].children[0].addEventListener('click', menuAct);
+}
